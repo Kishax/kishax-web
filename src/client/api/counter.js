79295a6aa34ef487aac27cca57edefde6b97b604
bella <maekawatakaya10.8@gmail.com>
2025-01-18 09:30:42 +0900

    describe text of showing at above numbers in type of datanames

--- src/client/api/counter.js
+++ src/client/api/counter.js
@@ -1,5 +1,6 @@
 // @ts-check
 import { Chart, registerables } from 'chart.js';
+// import { disnum } from '../../middlewares/counter';
 
 Chart.register(...registerables);
 
@@ -38,6 +39,7 @@ const DataType = {
  * @property {string} name
  * @property {string} color
  * @property {DataType} type
+ * @property {string} sumname
  */
 
 /**
@@ -52,17 +54,17 @@ async function fetchData(type) {
 
 /** @type {{ [key: string]: DataName }} */
 const datanames = {
-    loadcount: { name: 'ロード数', color: 'rgba(255, 0, 0, 1)', type: DataType.NON_ADMIN },
-    ipcount: { name: '訪問者数', color: 'rgba(0, 0, 255, 1)', type: DataType.COMMON },
-    adipcount: { name: '訪問者数(管理者含む)', color: 'rgba(0, 255, 0, 1)', type: DataType.ADMIN },
-    adloadcount: { name: 'ロード数(管理者含む)', color: 'rgba(255, 255, 0, 1)', type: DataType.ADMIN },
+    loadcount: { name: 'アクセス数', color: 'rgba(255, 0, 0, 1)', type: DataType.NON_ADMIN, sumname: '総アクセス数' },
+    ipcount: { name: '訪問者数', color: 'rgba(0, 0, 255, 1)', type: DataType.COMMON, sumname: '総訪問者数' },
+    adipcount: { name: '訪問者数(管理者含む)', color: 'rgba(0, 255, 0, 1)', type: DataType.ADMIN, sumname: '総訪問者数(管理者を含む)' },
+    adloadcount: { name: 'アクセス数(管理者含む)', color: 'rgba(255, 255, 0, 1)', type: DataType.ADMIN, sumname: '総アクセス数(管理者を含む)' },
 };
 
 /** @type {Chart | null} */
 let chart;
 
 /** @type {TotalCounts} */
-const totalCounts = {
+var totalCounts = {
     loadcount: 0,
     ipcount: 0,
     adloadcount: 0,
@@ -112,14 +114,31 @@ function createChart(data) {
             datasets: datasets
         },
         options: {
+            responsive: true,
             scales: {
                 y: {
                     beginAtZero: true
+                }
+            },
+            plugins: {
+                legend: {
+                    onClick: function(e, legendItem) {
+                        console.log('plugins.block!');
+                        const index = legendItem.datasetIndex;
+                        const ci = this.chart;
+                        if (index) {
+                            const meta = ci.getDatasetMeta(index);
+                        }
+                        console.log(`グラフの見出し: ${legendItem.text} がクリックされました。`)
+                    }
                 }
             }
         }
     });
 }
+
+/** @type {boolean} */
+var isAdminView = false;
 
 /**
  * @param  {...string} keys
@@ -135,7 +154,30 @@ function toggleDataset(...keys) {
         if (key in datanames) {
             const dataset = currentChart.data.datasets.find(ds => ds.label === datanames[key]['name']);
             if (dataset) {
-                dataset.hidden = !dataset.hidden;
+                if (!isAdminView) {
+                    // 含めるPUSH
+                    if (dataset.hidden) {
+                        dataset.hidden = !dataset.hidden;
+                    }
+                    const buttonDiv = document.getElementById('toggleAdcount');
+                    if (buttonDiv) {
+                        buttonDiv.textContent = '';
+                        const newText = document.createTextNode('管理者を除外する');
+                        buttonDiv.appendChild(newText);
+                    }
+                } else {
+                    // 省くPUSH
+                    if (!dataset.hidden) {
+                        dataset.hidden = !dataset.hidden;
+                    }
+                    const buttonDiv = document.getElementById('toggleAdcount');
+                    if (buttonDiv) {
+                        buttonDiv.textContent = '';
+                        const newText = document.createTextNode('管理者を含める');
+                        buttonDiv.appendChild(newText);
+                    }
+                }
+                isAdminView = !isAdminView;
             } else {
                 console.error(`Dataset not found: ${key}`);
             }
@@ -151,4 +193,22 @@ const type = urlParams.get('type') || 'month';
 
 fetchData(type).then(data => {
     createChart(data);
+    const ids = [ 'ipcount', 'loadcount', 'adipcount', 'adloadcount' ];
+
+    ids.forEach(id => {
+        /** @type {number} */
+        let countnum = totalCounts[id];
+        let elm = document.getElementById(id);
+        if (elm) {
+            elm.className = 'count-num';
+
+            elm.textContent = '';
+            const text = document.createTextNode(datanames[id]['sumname'] + ': ' + countnum.toString());
+            const br = document.createElement('br');
+            elm.appendChild(text);
+            elm.appendChild(br);
+        } else {
+            console.error('elm nothing!');
+        }
+    });
 }).catch(error => console.error('Error fetching data:', error));
