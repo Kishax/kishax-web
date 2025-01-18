3ff3469677e12044c47aac13c8d175b456cd8cfd
bella <maekawatakaya10.8@gmail.com>
2025-01-18 15:17:11 +0900

    add middleware counter

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,7 @@ import preErrorHandler from './middlewares/pre-error-handler';
 import csrfProtection from './middlewares/csurf';
 import mycors from './middlewares/cors';
 import errorHandler from './middlewares/error-handler';
+import counter from './middlewares/counter';
 
 const app = express();
 
@@ -42,6 +43,8 @@ app.use(passport.session());
 app.use(csrfProtection);
 
 app.use(localvals);
+
+app.use(counter);
 
 app.use(preErrorHandler);
 
