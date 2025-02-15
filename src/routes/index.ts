5394ed4d89efb814e5073388fa2e5a907577f153
bella <maekawatakaya10.8@gmail.com>
2025-02-15 12:49:49 +0900

    for: https://github.com/bella2391/FMCWebApp/commit/9df0f1671411319e52d9e4292c1c8a57257436c8 and https://github.com/bella2391/FMCWebApp/commit/eb343200d00018b73faa2ebe1d5895b93a0c8019

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,5 @@
 import express, { Request, Response, NextFunction } from 'express';
 import knex from '../config/knex';
-import basepath from '../utils/basepath';
 
 const router: express.Router = express.Router();
 
@@ -8,51 +7,15 @@ router.get('/', async (req: Request, res: Response, _: NextFunction) => {
     if (req.isAuthenticated()) {
         const user = req.user as any;
         const userId: number = user.id;
-        if (req.session.n) {
-            // mc/authに飛ばす
-            return;
-        }
-        knex('tasks')
-            .select("*")
-            .where({ user_id: userId })
-            .then((results) => {
-                res.render('index', {
-                    todos: results,
-                    isAuth: true,
-                });
-            })
-            .catch((err) => {
-                console.error(err);
-                res.render('index', {
-                    isAuth: true,
-                    errorMessage: [err.sqlMessage],
-                });
-            });
+        const newUser = await knex('users').where({ id: userId }).first();
+
+        res.render('index', {
+            isAuth: true,
+        });
     } else {
         res.render('index', {
             isAuth: false,
         });
-    }
-});
-
-router.post('/', async (req: Request, res: Response, _: NextFunction) => {
-    if (req.isAuthenticated()) {
-        //const userId: number = (req as Express.AuthenticatedRequest).user.id;
-        const userId: number = (req.user as any).id;
-        const todo: string = req.body.add;
-
-        knex("tasks")
-            .insert({ user_id: userId, content: todo })
-            .then(() => {
-                res.redirect(`${basepath.rootpath}/`);
-            })
-            .catch((err) => {
-                console.error(err);
-                res.render('index', {
-                    isAuth: true,
-                    errorMessage: [err.sqlMessage],
-                });
-            })
     }
 });
 
