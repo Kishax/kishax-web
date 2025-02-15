1817d2704a9e1df925003bbc3918d8e9347555e0
bella <maekawatakaya10.8@gmail.com>
2025-02-15 12:53:43 +0900

    in the process of making minecraft web confirmation site

---
+++ src/public/javascripts/alert.js
@@ -0,0 +1,3 @@
+document.getElementById("alert").addEventListener("click", () => {
+    window.alert("ログインが必要です。");
+});
---
+++ src/routes/mc.ts
@@ -0,0 +1,84 @@
+import express, { Request, Response } from 'express';
+import { FMCWebType } from '../@types/fmc';
+import knex, { mknex } from '../config/knex';
+
+const router: express.Router = express.Router();
+
+router.get('/auth', async (req: Request, res: Response) => {
+    if (req.query.n) {
+        const n: number = Number(req.query.n);
+        if (!isNaN(n)) {
+            req.session.n = n;
+        }
+    }
+
+    req.session.type = FMCWebType.MC_AUTH;
+
+    if (!req.isAuthenticated()) {
+        res.render('mc/auth', {
+            successMessage: [ 'WEB認証にはログインが必要です。' ],
+        });
+
+        return;
+    }
+
+    const user = req.user as any;
+    const userId: number = user.id;
+    const User = await knex('users').where({ id: userId }).first();
+
+    if (!req.session.n) {
+        res.render('mc/auth', {
+            successMessage: [ 'サーバーに参加しよう！' ],
+        });
+
+        return;
+    }
+
+    const mcuser = await mknex('members').where({ id: req.session.n }).first();
+
+    if (!mcuser) {
+        res.status(400).send('Invalid Access');
+        throw new Error('Invalid Access');
+    }
+
+    if (mcuser.confirm) {
+        res.status(400).send('Invalid Access');
+        throw new Error('Invalid Access');
+    }
+
+    const onlist = await mknex('status').where({ name: 'velocity'}).first();
+    if (!onlist) {
+        res.status(400).send('Database Error');
+        throw new Error('Not found where name is velocity in status table');
+    }
+
+    const playerlist = onlist.player_list;
+    if (!playerlist) {
+        res.render('mc/auth', {
+            errorMessage: [ 'プレイヤーがオンラインでないため、WEB認証ができません。' ],
+            username: User.name,
+        });
+    }
+
+    const onlinePlayers: [ string ] = playerlist.split(",");
+
+    if (!onlinePlayers.includes(mcuser.name)) {
+        res.render('mc/auth', {
+            errorMessage: [ 'プレイヤーがオンラインでないため、WEB認証ができません。' ],
+            username: User.name,
+        });
+    }
+
+    res.render('mc/auth', {
+        successMessage: [ 'プレイヤー情報が自動入力されました。' ],
+        username: User.name,
+        mcid: mcuser.name,
+        uuid: mcuser.uuid,
+    });
+});
+
+router.post('/auth', async (req: Request, res: Response) => {
+
+});
+
+export default router;
