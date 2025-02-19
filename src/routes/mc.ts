f19a94f6490d3e416332a6cefc954807732624a8
bella <maekawatakaya10.8@gmail.com>
2025-02-20 07:06:47 +0900

    add jwt and zod pkg use case

--- src/routes/mc.ts
+++ src/routes/mc.ts
@@ -1,6 +1,18 @@
 import express, { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+import { z } from 'zod';
+import '../config';
 import { FMCWebType } from '../@types/fmc';
 import knex, { mknex } from '../config/knex';
+
+const JWT_SECRET = process.env.JWT_SECRET || 'jwtsecret';
+
+const authSchema = z.object({
+    token: z.string(),
+    mcid: z.string(),
+    uuid: z.string(),
+    pass: z.string(),
+});
 
 const router: express.Router = express.Router();
 
@@ -81,17 +93,58 @@ router.get('/auth', async (req: Request, res: Response) => {
         return;
     }
 
-    res.render('mc/auth', {
-        successMessage: [ 'プレイヤー情報が自動入力されました。' ],
-        username: User.name,
-        mcid: mcuser.name,
-        uuid: mcuser.uuid,
-        mcAuth: true,
-    });
+    const payload: Jsonwebtoken.McAuthJwtPayload = { username: User.name, mcid: mcuser.name, uuid: mcuser.uuid };
+    try {
+        const token: string = jwt.sign(payload, JWT_SECRET, { expiresIn: '1h' });
+
+        res.render('mc/auth', {
+            successMessage: req.flash('successMessage') || [ 'プレイヤー情報が自動入力されました。' ],
+            username: User.name,
+            mcid: mcuser.name,
+            uuid: mcuser.uuid,
+            mcAuth: true,
+            token,
+        });
+    } catch (error) {
+        res.status(400).send('Can\'t generate token');
+        return;
+    }
 });
 
 router.post('/auth', async (req: Request, res: Response) => {
+    if (!req.isAuthenticated()) {
+        req.flash('errorMessage', [ 'ログインが必要です。' ]);
+        return res.redirect('/mc/auth');
+    }
 
+    const result = authSchema.safeParse(req.body);
+
+    if (!result.success) {
+        res.status(400).send('Invalid Access: result not success');
+        return;
+    }
+
+    const queryData = result.data;
+
+    try {
+        const payload = jwt.verify(queryData.token, JWT_SECRET) as Jsonwebtoken.McAuthJwtPayload;
+
+        if (queryData.mcid !== payload.mcid || queryData.uuid !== payload.uuid) {
+            res.status(400).send('Invalid Access: not correspond with payload and queryData');
+            return;
+        }
+
+        const dbInfo = await mknex('members').where({ name: payload.mcid, uuid: payload.uuid }).first();
+        if (dbInfo && typeof dbInfo.secret2 !== 'number') {
+            req.flash('errorMessage', [ 'マイクラサーバーで/retryコマンドよりワンタイムパスワードを更新してください。' ]);
+            return res.redirect('/mc/auth');
+        }
+
+    } catch (error) {
+        console.log('/mc/auth POST error:', error);
+        res.status(400).send('Invalid Access');
+        return;
+    }
 });
 
 export default router;
