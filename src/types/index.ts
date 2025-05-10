3dddb27c755246d6526419b2ab90c50ff13f48d7
bella <maekawatakaya10.8@gmail.com>
2025-05-10 23:47:20 +0900

    provide type definition for json

--- src/types/index.ts
+++ src/types/index.ts
@@ -20,9 +20,107 @@ type FlashRequire = FlashLocalVal | any;
 interface FlashParams {
   [key: string]: FlashRequire;
 }
+
+interface Config {
+  name: string;
+  year: number;
+  url: string;
+  og: {
+    title: string;
+    description: string;
+    image: string;
+    url: string;
+  }
+  social: {
+    me: {
+      portfolio: {
+        url: string
+      }
+    }
+    discord: {
+      kishax: {
+        url: string;
+      }
+    }
+    github: {
+      kishax: {
+        url: string;
+      }
+    }
+  }
+  server: {
+    port: string;
+    url: string;
+    protocol: string;
+    root: string;
+    host: string;
+    logo: {
+      url: string;
+    }
+    modules: {
+      jwt: {
+        secret: string;
+      }
+      mysql: {
+        host: string;
+        user: string;
+        password: string;
+        database: string;
+      }
+      passport: {
+        x: {
+          key: string;
+          secret: string;
+        }
+        google: {
+          key: string;
+          secret: string;
+        }
+        discord: {
+          key: string;
+          secret: string;
+        }
+      }
+      express_session: {
+        secret: string;
+      }
+      nodemailer: {
+        host: string;
+        port: string | number;
+        user: string;
+        password: string;
+      }
+      skyway: {
+        id: string;
+        secret: string;
+        session: {
+          token: string;
+        }
+      }
+      express: {
+        websocket: {
+          protocol: 'ws' | 'wss';
+          root: string;
+          url: string;
+          host: string;
+        }
+      }
+    }
+  }
+  client: {
+    icon: {
+      random: {
+        enable: boolean;
+      }
+      path: string;
+    }
+  }
+}
+
 export {
   WebType,
   FlashRequire,
+  Config,
   FlashParams,
   FlashLocalVal,
   FlashLocalPath,
